170cc467a56a73416c7144d76bca8e5da57904d9
Max Mossberg <mossberg.max@gmail.com>
2017-01-03 13:55:41 -0500

    day 5 part 2

---
+++ day_5pt2.py
@@ -0,0 +1,43 @@
+import hashlib
+import pdb
+import time
+
+string = 'uqwqemis'
+password = ['','','','','','','','']
+num = 0
+nums = []
+
+while '' in password:
+	fake = ['','','','','','','','']
+	hashed = hashlib.md5((string + str(num)).encode())
+	if '00000' == hashed.hexdigest()[0:5]:
+		if hashed.hexdigest()[5] in '1234567890':
+			if int(hashed.hexdigest()[5]) > 7:
+				pass
+			if int(hashed.hexdigest()[5]) < 8:
+				if password[int(hashed.hexdigest()[5])] == '':
+					password[int(hashed.hexdigest()[5])] = hashed.hexdigest()[6]
+					nums.append(num)
+				else:
+					pass
+		else:
+			pass
+		num += 1
+	else:
+#		if num%100000 == 0:
+#			for x in range(len(password)):
+#					fake[x] = password[x]
+#				else:
+#					fake[x] = hashed.hexdigest()[x]
+#			print(''.join(fake))
+		if num%5000000 == 0:
+			print(num)
+#			time.sleep(.25)
+		num += 1
+print('finished')
+print(password)
+print(nums)
+for x in nums:
+	hashed = hashlib.md5((string + str(x)).encode())
+	print(hashed.hexdigest())
+pdb.set_trace()
